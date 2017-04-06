753d6369cd0ecd9da18e976109af426b6705d24b
swizkon <jonas@jerndin.se>
2017-04-06 23:55:55 +0200

    New root folder to test git deploy to azure

---
+++ CircuitModule.cs
@@ -0,0 +1,31 @@
+namespace NancyApplication
+{
+    using Nancy;
+
+    using Nancy.ModelBinding;
+
+    using Endurance.Domain;
+    using Endurance.Domain.Model;
+
+    public class CircuitModule : NancyModule
+    {
+        public CircuitModule() : base("circuits")
+        {
+            Get("/list", args =>
+            {
+                var model = Repository.All();
+                return Response.AsJson(model);
+            });
+
+            Get("/about", _ => "This is the about page...");
+
+            Post("/create", args =>
+            {
+                var model = this.Bind<Circuit>();
+                // model.Name += Request.Body.Length + ""; // Request.Form["name"];
+                Repository.Add(model);
+                return Response.AsJson(model);
+            });
+        }
+    }
+}
---
+++ Domain/Repository.cs
@@ -0,0 +1,24 @@
+
+namespace Endurance.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Concurrent;
+
+    using Model;
+
+    public class Repository
+    {
+        private static ConcurrentDictionary<Guid, Circuit> circuits = new ConcurrentDictionary<Guid, Circuit>();
+
+        public static void Add(Circuit circuit)
+        {
+            circuits.AddOrUpdate(circuit.CircuitId, circuit, (key, obj) => circuit );
+        }
+ 
+        public static ICollection<Circuit> All()
+        {
+            return circuits.Values;
+        }
+    }
+}
